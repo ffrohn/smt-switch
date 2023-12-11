3f89fb64002903c1628c7eafba404c4a60fa48f6
Florian Frohn <florian.frohn@cs.rwth-aachen.de>
2023-12-11 17:10:03 +0100

    fix compile

--- z3/src/z3_solver.cpp
+++ z3/src/z3_solver.cpp
@@ -164,7 +164,7 @@ void Z3Solver::set_opt(const std::string option, const std::string value)
   {
     try
     {
-      const auto num = stoi(value);
+      const unsigned int num = stoi(value);
       slv.set(o, num);
     }
     catch (const std::invalid_argument&)
